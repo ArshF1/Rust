827f71a9d07361ad1dd788a085b191f6b2e96af1
Arsh Sahay <arsh.sahay0209@gmail.com>
2026-08-01 00:45:28 +0530

    formatted code

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,89 @@
 use std::fs;
 
-fn for_loop()-> () {
+fn for_loop() -> () {
     for i in 0..3 {
         println!("Loop value: {}", i);
     }
 }
 
-fn while_loop()-> () {
+fn while_loop() -> () {
     // We need to declare the var value as mutable in order to update it
-    let mut value=10;
-    while value>1 {
+    let mut value = 10;
+    while value > 1 {
         println!("Inverted loop value: {}", value);
-        value=value-1
+        value = value - 1
     }
 }
 
-fn conditionals()-> () {
-    let is_even=true;
+fn conditionals() -> () {
+    let is_even = true;
     if is_even {
         println!("It is even");
-    }
-    else {
+    } else {
         println!("It is odd");
     }
 }
 
-fn get_char_at_index(index:usize)-> () {
-    let word:String=String::from("Czechoslovakia");
-    let char=word.chars().nth(index);
+fn get_char_at_index(index: usize) -> () {
+    let word: String = String::from("Czechoslovakia");
+    let char = word.chars().nth(index);
 
     // This is called graceful handling
     match char {
-        Some(c)=> println!("The character at index {} is {}", index, c),
-        None=> println!("No character at index {}", index),
+        Some(c) => println!("The character at index {} is {}", index, c),
+        None => println!("No character at index {}", index),
     }
 }
 
-fn get_first_word(sentence:String)-> String {
-    let mut word=String::from("");
+fn get_first_word(sentence: String) -> String {
+    let mut word = String::from("");
     for char in sentence.chars() {
         word.push_str(&char.to_string().as_str());
-        if char==' ' {
-            break ;
+        if char == ' ' {
+            break;
         }
     }
     return word;
 }
 
-fn take_ownership()-> () {
-    let s1=String::from("Hi");
-    let s2=s1;
+fn take_ownership() -> () {
+    let s1 = String::from("Hi");
+    let s2 = s1;
 
-    let response:Result<String, String>=Ok(s2);
+    let response: Result<String, String> = Ok(s2);
 
     match response {
-        Ok(content)=> println!("Response ok {}", content),
-        Err(err)=> println!("Error: {}", err),
+        Ok(content) => println!("Response ok {}", content),
+        Err(err) => println!("Error: {}", err),
     }
 }
 
-fn error_handling()-> () {
-    let file_buffer=fs::read_to_string("test.txt");
+fn error_handling() -> () {
+    let file_buffer = fs::read_to_string("test.txt");
 
     match file_buffer {
-        Ok(content)=> println!("The contents of file are {}", content),
-        Err(err)=> println!("No file found. Error: {}", err),
+        Ok(content) => println!("The contents of file are {}", content),
+        Err(err) => println!("No file found. Error: {}", err),
     }
 }
 
-fn update_str_reference(str: &mut String)->String {
+fn update_str_reference(str: &mut String) -> String {
     // This is regarding a mutable reference
     str.push_str(" This is pushed right after sentence");
-    let response=str.to_string();
+    let response = str.to_string();
     println!("The updated sentence is '{}'", response);
-    return response
+    return response;
 }
 
-fn main(){
+fn main() {
     // Basic variables
-    let _str:String=String::from("Hi");
-    let _num:u8=255;
+    let _str: String = String::from("Hi");
+    let _num: u8 = 255;
 
     get_char_at_index(4);
     take_ownership();
     error_handling();
 
-    let mut sentence=String::from("Hi, this is a sentence");
+    let mut sentence = String::from("Hi, this is a sentence");
     update_str_reference(&mut sentence);
-
-
 }
