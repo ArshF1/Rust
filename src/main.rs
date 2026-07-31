b30e4611b940ac714ae795bfe8eb3bc3d32320e7
Arsh Sahay <arsh.sahay0209@gmail.com>
2026-07-31 23:56:42 +0530

    Code refactoring. More will be added

--- src/main.rs
+++ src/main.rs
@@ -1,181 +1,57 @@
-use std::fs;
-
-fn main() {
-    let mut num:u16=4;
-    print!("The number is {}\n", num);
-
-    for i in 1..5 {
-        if i%2==0 {
-            println!("{}",i);
-        }
+fn for_loop()-> () {
+    for i in 0..3 {
+        println!("Loop value: {}", i);
     }
+}
 
-    while num>2 {
-        println!("{}",num);
-        num=num-1;
+fn while_loop()-> () {
+    // We need to declare the var value as mutable in order to update it
+    let mut value=10;
+    while value>1 {
+        println!("Inverted loop value: {}", value);
+        value=value-1
     }
+}
 
-    let str=String::from("Hi, how are you?");
-    println!("The string to be printed is '{}'", str);
-
-    print!("Let's try printing the first letter of the string str\n");
-    let index=20;
-    let char=str.chars().nth(index);
-
-    // This is a safe way of getting a value at a particular index in a string
-    match char{
-        Some(c)=> println!("The {} character of '{}' is {}", index, str, c),
-        None=> println!("No character at {}", index),
-    }
-
-    // The unwrap function below will throw a runtime error, because of a non existend index
-    // println!("The {} character of '{}' is {}", index, str, char.unwrap());
-
-    // Conditionals
-    let is_even:bool=false;
-
+fn conditionals()-> () {
+    let is_even=true;
     if is_even {
-        println!("The number is even");
+        println!("It is even");
     }
-    else if !is_even {
-        println!("The number is not even");
+    else {
+        println!("It is odd");
     }
-
-    let sentence=String::from("Hello");
-    let first_word=get_first_word(sentence);
-    println!("The first word is '{}'", first_word);
-
-    let num1=4;
-    let num2=6;
-
-    let total=add(num1, num2);
-    println!("The sum of {} and {} is {}", num1, num2, total);
-    multiply(num1, num2);
-    
-
-    println!("===============================");
-    // Ownership
-    // In the below example, s1 had it's own owner, when we wrote s2=s1, s1 went out of scope, now the ownership got transferred from s1 to s2
-    // If you need the behaviour of s2=s1, you will have to use s1.clone(), it creates an entirely new variables
-    // This happens only for the heap memory
-    let s1=String::from("Hi, I am sentence 1");
-    let s2=s1;
-    // println!("This is Sentence 1 {}", s1);
-    println!("Sentence 1 is being borrowed by s2, '{}'", s2);
-    
-    // This behaviour is the same for functions as well
-    let s3=String::from("Hi, I am sentence 3");
-    println!("The string s3 is stored at pointer-> {:p}", &s3);
-    println!("Before transfer: {}", s3);
-    let _owner=take_ownership(s3);
-    // println!("After transfer: {}", s3);
-    println!("===============================");
-    
-    // Borrowing and references
-    // Mutable references
-
-    // Here to update a variable in a heap (in this case s4), you need to provide a mutable reference
-    let mut s4=String::from("I am sentence 4.");
-    update_str(&mut s4);
-
-    // Structs
-    struct User {
-        name: String,
-        age: u8,
-        active: bool
-    }
-    let name=String::from("arsh");
-    let user_details: User= User { name: name, age: 25, active: true };
-    println!("Name is {}, age is {}, He/She is active-> {}", user_details.name, user_details.age, user_details.active);
-
-
-    struct Rectangle {
-        height: u32,
-        width: u32
-    }
-
-    impl Rectangle {
-        fn perimater(&self)->u32 {
-            println!("The perimeter of the rectangle is {}", 2*(self.height+self.width));
-            return 2*(self.height+self.width);
-        }
-    }
-
-    let rect: Rectangle= Rectangle { height: 32, width: 20 };
-    rect.perimater();
-
-    // Enums
-    #[derive(Debug)]
-    enum Direction {
-        North,
-        South,
-        East,
-        West
-    }
-    let direction_n: Direction=Direction::North;
-    let direction_s: Direction=Direction::South;
-    println!("The direction is {:?}.", direction_n);
-    println!("The direction is {:?}.", direction_s);
-    // Need to understand enums
-
-    // Pattern matching (a switch case statement or a match case statement in python, it's the same thing)
-    enum Shape {
-        Circle(f64),
-        Square(f64),
-        Rectangle(f64, f64)
-    }
-
-    fn calculate_area(shape:Shape)-> f64 {
-        match shape {
-            Shape::Circle(radius)=> 3.14*radius*radius,
-            Shape::Square(side_length)=> side_length*side_length,
-            Shape::Rectangle(width, height)=> width*height,
-        }
-    }
-
-    // Error handling (The way an error is handled in rust. It is handled in the same format as the enum below)
-    // enum Result<A, B> {
-    //     Ok(A),
-    //     Err(B)
-    // }
-
-    let res=fs::read_to_string("example.txt");
-
-    match res {
-        Ok(content)=> println!("The contents of the file are {}", content),
-        Err(err)=> println!("Error: {}", err),
-    }
-
-
-
 }
 
-fn update_str(str: &mut String)-> () {
-    str.push_str("Pushed right after sentence 4");
+fn get_char_at_index(index:usize)-> () {
+    let word:String=String::from("Czechoslovakia");
+    let char=word.chars().nth(index);
+
+    // This is called graceful handling
+    match char {
+        Some(c)=> println!("The character at index {} is {}", index, c),
+        None=> println!("No character at index {}", index),
+    }
 }
 
-fn take_ownership(str:String)-> () {
-    println!("{}", str);
-}
-
-fn get_first_word(sentence:String) -> String {
-    let mut ans=String::from("");
+fn get_first_word(sentence:String)-> String {
+    let mut word=String::from("");
     for char in sentence.chars() {
-        ans.push_str(char.to_string().as_str());
-        if char == ' ' {
-            break;
+        word.push_str(&char.to_string().as_str());
+        if char==' ' {
+            break ;
         }
     }
-    return  ans;
+    return word;
 }
 
-fn add(num1:u16, num2:u16)->u16 {
-    let total:u16=num1+num2;
-    return total;
-}
+fn main(){
+    // Basic variables
+    let str:String=String::from("Hi");
+    let num:u8=255;
 
-fn multiply(num1:u16, num2:u16) ->u16 {
-    let total=num1*num2;
-    println!("The multiplication of {}x{} is {}", num1, num2, total);
-    return total;
+    get_char_at_index(4);
+
+
+
 }
